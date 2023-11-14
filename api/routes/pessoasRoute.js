a428d78d9bb0c409d8aa38a7bb9f9c498bf09e29
danielcarvalho77 <danielcarrosa@gmail.com>
2023-11-13 22:09:34 -0300

    orm projeto escola

---
+++ api/routes/pessoasRoute.js
@@ -0,0 +1,12 @@
+const {Router} = require('express');
+const PessoaController = require('../controllers/PessoaController');
+
+const router = Router()
+
+router.get('/pessoas', PessoaController.pegaTodasAsPessoas);
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa);
+router.post('/pessoas', PessoaController.criaPessoa);
+router.put('/pessoas/:id', PessoaController.atualizaUmaPessoa);
+router.delete('/pessoas/:id', PessoaController.deletaUmaPessoa);
+
+module.exports = router;
